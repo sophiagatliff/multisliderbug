1ae816474c1fc83c5a9fc013a423f6c4d2514b69
Sophia John Gatliff <sophia@thegrowingunion.com>
2020-12-01 14:38:26 -0500

    making the bug work, lol

--- doubleSliderBUg/ViewController.swift
+++ doubleSliderBUg/ViewController.swift
@@ -6,12 +6,24 @@
 //
 
 import UIKit
+import MultiSlider
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    @IBOutlet weak var multiSlider: MultiSlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        multiSlider.addTarget(self, action: #selector(sliderChanged(_:)), for: .valueChanged)
+        multiSlider.showsThumbImageShadow = false
+        multiSlider.thumbImage = multiSlider.thumbImage?.withTintColor(.systemIndigo)
+        multiSlider.valueLabelPosition = .top
+        multiSlider.isValueLabelRelative = false
+    }
+    
+    @objc func sliderChanged(_ slider: MultiSlider) {
     }
 
 
